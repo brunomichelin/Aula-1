8816709dac7301ed3d723fe1d43050c665301825
Bruno Henrique Michelin Silva <michelin.brunohenrique@hotmail.com>
2020-08-21 23:42:18 -0300

    Adicionar arquivos de projeto.

---
+++ Aula 1/Aula 1/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Aula_1.Models;
+
+namespace Aula_1.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
